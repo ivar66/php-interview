841d2ba8f297b38c2244b904c1c0cdda4776fe31
jack-dukang <625079860@qq.com>
2020-05-28 14:14:31 +0800

    fix add code

--- example/1.php
+++ example/1.php
@@ -1,5 +1,27 @@
 <?php
 
+/**
+ * @param Integer $m
+ * @param Integer $n
+ * @return Integer
+ */
+function uniquePaths($m, $n) {
+    $dp = [];
+    for($i = 1;$i<=$m;$i++){
+        $dp[$i][1] = 1;
+    }
+    for($j = 1;$j<=$n;$j++){
+        $dp[1][$j] = 1;
+    }
+    for($i = 2;$i<=$m;$i++){
+        for($j =2 ;$j<=$n;$j++){
+            $dp[$i][$j] = $dp[$i-1][$j] + $dp[$i][$j-1];
+        }
+    }
+
+    return $dp[$m][$n];
+}
+
 class Car{
     // 下面是车的属性
     private $color;
